05b45d2a718abcf042e5048a65364211f49bb3fe
Swastik Shrivastava <06swastik@gmail.com>
2020-05-12 18:47:08 +0530

    changes

---
+++ test_Calculator.py
@@ -0,0 +1,13 @@
+from Calculator import *
+
+def test_sum():
+    assert sum(5,3) == 8
+
+def test_diff():
+    assert diff(4,3) == 1
+
+def test_multiply():
+    assert multiply(2,3) == 6
+
+def test_divide():
+    assert divide(4,2) == 2
